b663e1299ebc7ca67845aae10cf854f5762d26e3
adamz <58021562+ad4mny@users.noreply.github.com>
2021-12-25 20:28:01 +0800

    updated attendance view for different roles

--- www/ajax/attendance.js
+++ www/ajax/attendance.js
@@ -1,72 +1,153 @@
 window.addEventListener('load', (event) => {
 
-    $.ajax({
-        url: webURL + "api/get_attendance",
-        dataType: 'json',
-        beforeSend: function () {
-            $('#load_gif').show();
-        },
-        success: function (data) {
+    if (token.role == "0") {
 
-            var today = new Date();
-            var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
-            var print_display;
+        $.ajax({
+            type: "POST",
+            url: webURL + "api/get_parent_attendance",
+            data: {
+                uid: token.userid
+            },
+            dataType: 'JSON',
+            beforeSend: function () {
+                $('#load_gif').show();
+            },
+            success: function (data) {
 
-            if (data != "") {
+                var today = new Date();
+                var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
+                var print_display;
 
-                for (var i = 0; i < data.length; i++) {
+                if (data != "") {
 
-                    print_display =
-                        '<div class="row pt-2">' +
-                        '<div class="col">' +
-                        '<p class="text-capitalize mb-0 fw-bold">' + data[i]['date'] + '</p>' +
-                        '</div>' +
-                        '</div>';
+                    for (var i = 0; i < data.length; i++) {
 
-
-                    // var attendenceid = data[i]['attendenceid'].split(',');
-                    var fullname = data[i]['fullname'].split(',');
-                    var status = data[i]['status'].split(',');
-                    var time = data[i]['time'].split(',');
-
-                    for (var x = 0; x < fullname.length; x++) {
-                        print_display +=
-                            ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
-                            '    <div class="col-6">' +
-                            '        <p class="text-truncate text-capitalize mb-0">' + fullname[x] + '</p>' +
-                            '    </div>' +
-                            '    <div class="col">' +
-                            '        <p class="text-capitalize mb-0">' + status[x] + '</p>' +
-                            '    </div>' +
-                            '    <div class="col">' +
-                            '        <p class="text-capitalize mb-0">' + time[x] + '</p>' +
-                            '    </div>' +
+                        print_display =
+                            '<div class="row pt-2">' +
+                            '<div class="col">' +
+                            '<p class="text-capitalize mb-0 fw-bold">' + data[i]['date'] + '</p>' +
+                            '</div>' +
                             '</div>';
-                    }
 
-                    if (data[i]['date'] == date) {
-                        $('#display').append(print_display);
-                    } else {
-                        $('#history').append(print_display);
+
+                        // var attendenceid = data[i]['attendenceid'].split(',');
+                        var fullname = data[i]['fullname'].split(',');
+                        var status = data[i]['status'].split(',');
+                        var time = data[i]['time'].split(',');
+
+                        for (var x = 0; x < fullname.length; x++) {
+                            print_display +=
+                                ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
+                                '    <div class="col-6">' +
+                                '        <p class="text-truncate text-capitalize mb-0">' + fullname[x] + '</p>' +
+                                '    </div>' +
+                                '    <div class="col">' +
+                                '        <p class="text-capitalize mb-0">' + status[x] + '</p>' +
+                                '    </div>' +
+                                '    <div class="col">' +
+                                '        <p class="text-capitalize mb-0">' + time[x] + '</p>' +
+                                '    </div>' +
+                                '</div>';
+                        }
+
+                        if (data[i]['date'] == date) {
+                            $('#display').append(print_display);
+                        } else {
+                            $('#history').append(print_display);
+                        }
                     }
+                } else {
+                    $('#display').append(
+                        ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
+                        '    <div class="col">' +
+                        '        <p class="text-capitalize mb-0">No attendance data available.</p>' +
+                        '    </div>' +
+                        '</div>'
+                    );
                 }
-            } else {
-                $('#display').append(
-                    ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
-                    '    <div class="col">' +
-                    '        <p class="text-capitalize mb-0">No attendance data available.</p>' +
-                    '    </div>' +
-                    '</div>'
-                );
+            },
+            error: function () {
+                $('#info_box').html('<div class="row"><div class="col"><p class="my-3 text-muted">Internal server error, please reload.</p></div></div>');
+            },
+            complete: function () {
+                $('#load_gif').hide();
             }
-        },
-        error: function () {
-            $('#info_box').html('<div class="row"><div class="col"><p class="my-3 text-muted">Internal server error, please reload.</p></div></div>');
-        },
-        complete: function () {
-            $('#load_gif').hide();
-        }
-    });
+        });
+
+    } else {
+
+        $('#scanner').show();
+
+        $.ajax({
+            url: webURL + "api/get_attendance",
+            dataType: 'json',
+            beforeSend: function () {
+                $('#load_gif').show();
+            },
+            success: function (data) {
+
+                var today = new Date();
+                var date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
+                var print_display;
+
+                if (data != "") {
+
+                    for (var i = 0; i < data.length; i++) {
+
+                        print_display =
+                            '<div class="row pt-2">' +
+                            '<div class="col">' +
+                            '<p class="text-capitalize mb-0 fw-bold">' + data[i]['date'] + '</p>' +
+                            '</div>' +
+                            '</div>';
+
+
+                        // var attendenceid = data[i]['attendenceid'].split(',');
+                        var fullname = data[i]['fullname'].split(',');
+                        var status = data[i]['status'].split(',');
+                        var time = data[i]['time'].split(',');
+
+                        for (var x = 0; x < fullname.length; x++) {
+                            print_display +=
+                                ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
+                                '    <div class="col-6">' +
+                                '        <p class="text-truncate text-capitalize mb-0">' + fullname[x] + '</p>' +
+                                '    </div>' +
+                                '    <div class="col">' +
+                                '        <p class="text-capitalize mb-0">' + status[x] + '</p>' +
+                                '    </div>' +
+                                '    <div class="col">' +
+                                '        <p class="text-capitalize mb-0">' + time[x] + '</p>' +
+                                '    </div>' +
+                                '</div>';
+                        }
+
+                        if (data[i]['date'] == date) {
+                            $('#display').append(print_display);
+                        } else {
+                            $('#history').append(print_display);
+                        }
+                    }
+                } else {
+                    $('#display').append(
+                        ' <div class="row rounded-3 shadow-sm border bg-white mb-1 mx-1">' +
+                        '    <div class="col">' +
+                        '        <p class="text-capitalize mb-0">No attendance data available.</p>' +
+                        '    </div>' +
+                        '</div>'
+                    );
+                }
+            },
+            error: function () {
+                $('#info_box').html('<div class="row"><div class="col"><p class="my-3 text-muted">Internal server error, please reload.</p></div></div>');
+            },
+            complete: function () {
+                $('#load_gif').hide();
+            }
+        });
+
+    }
+
 
 });
 
